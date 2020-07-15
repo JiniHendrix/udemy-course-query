4f09b0d7c8e3f4056292e0653fd2d531b29a59c9
root <root@DESKTOP-AUM85MI.localdomain>
2020-07-15 14:16:49 -0700

    made query get all events on start up from event bus

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const axios = require('axios')
 const cors = require('cors')
 
 const app = express()
 app.use(bodyParser.json())
 app.use(cors())
+
+const handleEvent = (type, data) => {
+  if (type === 'PostCreated') {
+    const { id, title } = data
+
+    posts[id] = { id, title, comments: [] }
+  }
+
+  if (type === 'CommentCreated') {
+    const { id, content, postId, status } = data
+
+    posts[postId].comments.push({ id, content, status })
+  }
+
+  if (type === 'CommentUpdated') {
+    const { id, postId, status } = data
+
+    const comment = posts[postId].comments.find(comment => comment.id === id)
+
+    comment.status = status
+  }
+}
 
 const posts = {}
 
@@ -15,19 +38,19 @@ app.get('/posts', (req, res) => {
 app.post('/events', (req, res) => {
   const { type, data } = req.body
 
-  if (type === 'PostCreated') {
-    const { id, title } = data
-
-    posts[id] = { id, title, comments: [] }
-  }
-
-  if (type === 'CommentCreated') {
-    const { id, content, postId } = data
-
-    posts[postId].comments.push({ id, content })
-  }
+  handleEvent(type, data)
 
   res.send()
 })
 
-app.listen(4002, () => { console.log('Listening on 4002')})
+app.listen(4002, async () => {
+  console.log('Query service listening on 4002')
+
+  const res = await axios.get('http://localhost:4005/events')
+
+  for (let event of res.data) {
+    console.log('Processing event: ', event.type)
+
+    handleEvent(event.type, event.data)
+  }
+})
